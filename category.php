c6168fb8e8ecddab9bb7c185b3bc14eab8700aed
goldipl <mgodlewski92@gmail.com>
2023-10-28 13:09:19 +0200

    correct title

--- category.php
+++ category.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>PROMOTECA | Strona kategorii</title>
+        <title>Spark Promotions | Strona kategorii</title>
         <link rel="shortcut icon" href="./assets/icons/favicon.jpg" type="image/x-icon">
         <link rel="stylesheet" href="./scss/main.css">
     </head>
