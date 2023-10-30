8609e69a73f2da1b0aaa1b0bb47eff2abb008537
goldipl <mgodlewski92@gmail.com>
2023-10-30 17:44:20 +0100

    styling products blocks & tabs

--- components/main_page/popular_categories.php
+++ components/main_page/popular_categories.php
@@ -11,10 +11,108 @@
             </label>
         </div>
         <div class="tab">
+            <div class="products_blocks_wrapper">
+                <div class="products_blocks_wrapper__slot">
+                    <div class="products_block_wrapper__slot--img">
+                        <img src="./assets/img/products/pendrive.jpg">
+                    </div>
+                    <div class="products_block_wrapper__slot--title">
+                        <p>USB 32GB Pierre Cardin</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--colors">
+        
+                    </div>
+                    <div class="products_block_wrapper__slot--price">
+                        <p class="price">Cena od <span>100,90</span> zł</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--btn">
+                        <a href="#">Więcej</a>
+                    </div>
+                </div>
 
+                <div class="products_blocks_wrapper__slot">
+                    <div class="products_block_wrapper__slot--img">
+                        <img src="./assets/img/products/pendrive.jpg">
+                    </div>
+                    <div class="products_block_wrapper__slot--title">
+                        <p>Antystres "kostka" V2704-08</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--colors">
+        
+                    </div>
+                    <div class="products_block_wrapper__slot--price">
+                        <p class="price">Cena od <span>100,90</span> zł</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--btn">
+                        <a href="#">Więcej</a>
+                    </div>
+                </div>
+
+                <div class="products_blocks_wrapper__slot">
+                    <div class="products_block_wrapper__slot--img">
+                        <img src="./assets/img/products/pendrive.jpg">
+                    </div>
+                    <div class="products_block_wrapper__slot--title">
+                        <p>Antystres "kostka" V2704-08</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--colors">
+                        <div class="color_square white"></div>
+                        <div class="color_square darkblue"></div>
+                        <div class="color_square red"></div>
+                        <div class="color_square yellow"></div>
+                    </div>
+                    <div class="products_block_wrapper__slot--price">
+                        <p class="price">Cena od <span>100,90</span> zł</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--btn">
+                        <a href="#">Więcej</a>
+                    </div>
+                </div>
+
+                <div class="products_blocks_wrapper__slot">
+                    <div class="products_block_wrapper__slot--img">
+                        <img src="./assets/img/products/pendrive.jpg">
+                    </div>
+                    <div class="products_block_wrapper__slot--title">
+                        <p>Antystres "kostka" V2704-08</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--colors">
+                        <div class="color_square white"></div>
+                        <div class="color_square darkblue"></div>
+                        <div class="color_square red"></div>
+                        <div class="color_square yellow"></div>
+                        <div class="color_square green"></div>
+                        <div class="color_square purple"></div>
+                    </div>
+                    <div class="products_block_wrapper__slot--price">
+                        <p class="price">Cena od <span>100,90</span> zł</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--btn">
+                        <a href="#">Więcej</a>
+                    </div>
+                </div>
+
+                <div class="products_blocks_wrapper__slot">
+                    <div class="products_block_wrapper__slot--img">
+                        <img src="./assets/img/products/pendrive.jpg">
+                    </div>
+                    <div class="products_block_wrapper__slot--title">
+                        <p>Antystres "kostka" V2704-08</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--colors">
+        
+                    </div>
+                    <div class="products_block_wrapper__slot--price">
+                        <p class="price">Cena od <span>100,90</span> zł</p>
+                    </div>
+                    <div class="products_block_wrapper__slot--btn">
+                        <a href="#">Więcej</a>
+                    </div>
+                </div>
+            </div>
         </div>
         <input type="radio" id="tab_2" name="the_tabs">
-        <div class="tab-text second">
+        <div class="tab-text">
             <label for="tab_2">
                 <img src="./assets/icons/tabs/office-items.svg" alt="icon" width="40" height="40">
                 <p>Artykuły<br> biurowe</p>
@@ -34,7 +132,7 @@
       
       </div>
         <input type="radio" id="tab_4" name="the_tabs">
-        <div class="tab-text second">
+        <div class="tab-text">
             <label for="tab_4">
                 <img src="./assets/icons/tabs/clothes.svg" alt="icon" width="40" height="40">
                 <p>Odzież i<br> tekstylia</p>
@@ -44,7 +142,7 @@
       
       </div>
         <input type="radio" id="tab_5" name="the_tabs">
-        <div class="tab-text">
+        <div class="tab-text last">
             <label for="tab_5">
                 <img src="./assets/icons/tabs/lanyards.svg" alt="icon" width="40" height="40">
                 <p>Smycze i<br> opaski na rękę</p>
