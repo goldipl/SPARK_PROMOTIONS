a5c8c5f7f5693c7d8740564c58b041e33171320d
goldipl <mgodlewski92@gmail.com>
2023-10-30 16:32:32 +0100

    add tabs template

--- components/main_page/popular_categories.php
+++ components/main_page/popular_categories.php
@@ -1,3 +1,57 @@
 <section id="popular_categories">
-    <h1 class="title"><span>Popularne</span> kategorie</h1>
+    <h1 class="title">
+        <span>Popularne</span> kategorie
+    </h1>
+    <div class="the_tabs">
+        <input type="radio" id="tab_1" name="the_tabs" checked="checked">
+        <div class="tab-text">
+            <label for="tab_1">
+                <img src="./assets/icons/tabs/bestseller.svg" alt="icon" width="40" height="40">
+                <p>Artykuły biurowe</p>
+            </label>
+        </div>
+        <div class="tab">
+          
+        </div>
+        <input type="radio" id="tab_2" name="the_tabs">
+        <div class="tab-text second">
+            <label for="tab_2">
+                <img src="./assets/tabs-icons/headbands.svg" alt="icon" width="40" height="80">
+                <p>Opaski</p>
+            </label>
+        </div>
+        <div class="tab">
+      
+        </div>
+        <input type="radio" id="tab_3" name="the_tabs">
+        <div class="tab-text">
+            <label for="tab_3">
+                <img src="./assets/tabs-icons/bags.svg" alt="icon" width="40" height="80">
+                <p>Torby reklamowe</p>
+            </label>
+        </div>
+        <div class="tab">
+      
+      </div>
+        <input type="radio" id="tab_4" name="the_tabs">
+        <div class="tab-text second">
+            <label for="tab_4">
+                <img src="./assets/tabs-icons/keychains.svg" alt="icon" width="40" height="80">
+                <p>Breloki</p>
+            </label>
+        </div>
+        <div class="tab">
+      
+      </div>
+        <input type="radio" id="tab_5" name="the_tabs">
+        <div class="tab-text">
+            <label for="tab_5">
+                <img src="./assets/tabs-icons/cosmetics.svg" alt="icon" width="40" height="80">
+                <p>Kosmetyki</p>
+            </label>
+        </div>
+        <div class="tab">
+      
+        </div>
+    </div>
 </section>
