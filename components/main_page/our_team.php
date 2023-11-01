413c820a40b3f49a759464eb25e0eed237728442
goldipl <mgodlewski92@gmail.com>
2023-11-01 16:57:10 +0100

    fill our team template

--- components/main_page/our_team.php
+++ components/main_page/our_team.php
@@ -1,3 +1,103 @@
 <section id="our_team">
-    
+    <h1 class="title">Nasz <span>zespół</span></h1>
+    <p class="desc">Doradzą, polecą, przygotują ofertę, a także chętnie odpowiedzą na wszystkie Państwa pytania. Nasz zespół to dziesięć barwnych osobowości, które dzięki swojemu doświadczeniu w branży reklamowej będą profesjonalnym wsparciem w promowaniu Państwa firmy.</p>
+    <div class="our_team_container">
+        <div class="our_team_container__slot">
+            <div class="our_team_container__slot--image">
+                <img src="./assets/img/our_team/monika-karkuszewska.jpg" alt="team image" width="123" height="176">
+            </div>
+            <div class="our_team_container__slot--text">
+                <p class="name">Monika Karkuszewska</p>
+                <p class="position">Key Account Manager</p>
+                <p class="tel">Kom.: <a href="tel:+48 604 950 951">+48 604 950 951</a></p>
+                <p class="mail"><a href="mailto:karkuszewska@sparkpromotions.pl">karkuszewska@sparkpromotions.pl</a></p>
+                <p class="qrcode"><a href="">Otwórz kod QR</a></p>
+            </div>
+        </div>
+        <div class="our_team_container__slot">
+            <div class="our_team_container__slot--image">
+                <img src="./assets/img/our_team/wojciech-czepiel.jpg" alt="team image" width="123" height="176">
+            </div>
+            <div class="our_team_container__slot--text">
+                <p class="name">Wojciech Czepiel</p>
+                <p class="position">Key Account Manager</p>
+                <p class="tel">Kom.: <a href="tel:+48 600 983 500">+48 600 983 500</a></p>
+                <p class="mail"><a href="mailto:czepiel@sparkpromotions.pl">czepiel@sparkpromotions.pl</a></p>
+                <p class="qrcode"><a href="">Otwórz kod QR</a></p>
+            </div>
+        </div>
+        <div class="our_team_container__slot">
+            <div class="our_team_container__slot--image">
+                <img src="./assets/img/our_team/urszula-cwiklinska.jpg" alt="team image" width="123" height="176">
+            </div>
+            <div class="our_team_container__slot--text">
+                <p class="name">Urszula Ćwiklińska</p>
+                <p class="position">Key Account Manager</p>
+                <p class="tel">Kom.: <a href="tel:+48 881 944 928">+48 881 944 928</a></p>
+                <p class="mail"><a href="mailto:cwiklinska@sparkpromotions.pl">cwiklinska@sparkpromotions.pl</a></p>
+                <p class="qrcode"><a href="">Otwórz kod QR</a></p>
+            </div>
+        </div>
+        <div class="our_team_container__slot">
+            <div class="our_team_container__slot--image">
+                <img src="./assets/img/our_team/bogumil-piniarski.jpg" alt="team image" width="123" height="176">
+            </div>
+            <div class="our_team_container__slot--text">
+                <p class="name">Bogumił Piniarski</p>
+                <p class="position">Key Account Manager</p>
+                <p class="tel">Kom.: <a href="tel:+48 602 585 500">+48 602 585 500</a></p>
+                <p class="mail"><a href="mailto:piiniarsk@sparkpromotions.pl">piiniarsk@sparkpromotions.pl</a></p>
+                <p class="qrcode"><a href="">Otwórz kod QR</a></p>
+            </div>
+        </div>
+        <div class="our_team_container__slot">
+            <div class="our_team_container__slot--image">
+                <img src="./assets/img/our_team/marek-ziolkowski.jpg" alt="team image" width="123" height="176">
+            </div>
+            <div class="our_team_container__slot--text">
+                <p class="name">Marek Ziółkowski</p>
+                <p class="position">Key Account Manager</p>
+                <p class="tel">Tel.: <a href="tel:+48 71 326 74 12">+48 71 326 74 12</a></p>
+                <p class="tel">Kom.: <a href="tel:+48 795 599 178">+48 795 599 178</a></p>
+                <p class="mail"><a href="mailto:ziolkowski@sparkpromotions.pl">ziolkowski@sparkpromotions.pl</a></p>
+                <p class="qrcode"><a href="">Otwórz kod QR</a></p>
+            </div>
+        </div>
+        <div class="our_team_container__slot">
+            <div class="our_team_container__slot--image">
+                <img src="./assets/img/our_team/agata-czopek.jpg" alt="team image" width="123" height="176">
+            </div>
+            <div class="our_team_container__slot--text">
+                <p class="name">Agata Czopek</p>
+                <p class="position">Key Account Manager</p>
+                <p class="tel">Kom.: <a href="tel:+48 600 983 500">+48 600 983 500</a></p>
+                <p class="mail"><a href="mailto:karkuszewska@sparkpromotions.pl">karkuszewska@sparkpromotions.pl</a></p>
+                <p class="qrcode"><a href="">Otwórz kod QR</a></p>
+            </div>
+        </div>
+        <div class="our_team_container__slot">
+            <div class="our_team_container__slot--image">
+                <img src="./assets/img/our_team/witold-piasecki.jpg" alt="team image" width="123" height="176">
+            </div>
+            <div class="our_team_container__slot--text">
+                <p class="name">Witold Piasecki</p>
+                <p class="position">Key Account Manager</p>
+                <p class="tel">Kom.: <a href="tel:+48 881 944 928">+48 881 944 928</a></p>
+                <p class="mail"><a href="mailto:karkuszewska@sparkpromotions.pl">karkuszewska@sparkpromotions.pl</a></p>
+                <p class="qrcode"><a href="">Otwórz kod QR</a></p>
+            </div>
+        </div>
+        <div class="our_team_container__slot">
+            <div class="our_team_container__slot--image">
+                <img src="./assets/img/our_team/monika-karkuszewska.jpg" alt="team image" width="123" height="176">
+            </div>
+            <div class="our_team_container__slot--text">
+                <p class="name">Barbara Floryn</p>
+                <p class="position">Office Manager</p>
+                <p class="tel">Kom.: <a href="tel:+48 602 585 500">+48 602 585 500</a></p>
+                <p class="mail"><a href="mailto:piiniarsk@sparkpromotions.pl">piiniarsk@sparkpromotions.pl</a></p>
+                <p class="qrcode"><a href="">Otwórz kod QR</a></p>
+            </div>
+        </div>
+    </div>
 </section>
