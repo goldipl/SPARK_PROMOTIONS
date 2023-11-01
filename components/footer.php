303880c9388788be73fcc2b208a49177ba8b8e73
goldipl <mgodlewski92@gmail.com>
2023-11-01 13:04:35 +0100

    fill footer template

--- components/footer.php
+++ components/footer.php
@@ -1,8 +1,76 @@
-<div class="footer-container">
-    <div class="footer-top">
-  
+<div class="footer">
+    <div class="footer_top">
+        <div class="footer_top-col">
+            <div class="footer_top-col--title">
+                <p>Popularne kategorie</p>
+            </div>
+            <div class="footer_top-col--text">
+                <a href="#">Do picia</a>
+            </div>
+            <div class="footer_top-col--text">
+                <a href="#">Długopisy i zestawy piśmienne</a>
+            </div>
+            <div class="footer_top-col--text">
+                <a href="#">Odzież i tekstylia</a>
+            </div>
+            <div class="footer_top-col--text">
+                <a href="#">Latarki i narzędzia</a>
+            </div>
+            <div class="footer_top-col--text">
+                <a href="#">Pamięci USB</a>
+            </div>
+            <div class="footer_top-col--text">
+                <a href="#">Sport i rekreacja</a>
+            </div>
+            <div class="footer_top-col--text">
+                <a href="#">Artykuły biurowe</a>
+            </div>
+            <div class="footer_top-col--text">
+                <a href="#">Dyski</a>
+            </div>
+        </div>
+        <div class="footer_top-col">
+            <div class="footer_top-col--title">
+                <p>Kontakt</p>
+            </div>
+            <div class="footer_top-col--text telephone">
+                <a href="tel:+48 71 326 74 10">+48 71 326 74 10</a>
+            </div>
+            <div class="footer_top-col--text mail">
+                <a href="mailto:wroclaw@sparkpromotions.pl">wroclaw@sparkpromotions.pl</a>
+            </div>
+            <div class="footer_top-col--text hours">
+                <p class="small-text">9:00 - 17:00</p>
+            </div>
+            <div class="footer_top-col--text location">
+                <p class="small-text">Spark Promotions Sp. z o.o. Sp. k. <br>ul. Św. Jerzego 1A<br>50-518 Wrocław<br>NIP: 8951788428</p>
+            </div>
+        </div>
+        <div class="footer_top-col">
+            <div class="footer_top-col--title">
+                <p>Newsletter</p>
+            </div>
+            <div class="newsletter_footer_container">
+                <input type="text" name="newsletter" id="newsletter_input" placeholder="Wpisz adres e-mail">
+                <input type="button" id="newsletter_btn" value="Zapisz się">
+            </div>
+            <div class="footer_top-col--text">
+                <p class="small-text">Zapisz się do NEWSLETTERA i otrzymuj <br>na bieżąco informacje o naszych <br>promocjach i wyprzedażach.</p>
+            </div>
+        </div>
+        <div class="footer_top-col">
+            <div class="footer_top-col--title">
+                <p>Nagrody i certyfikaty</p>
+            </div>
+            <div class="footer_top-col--img">
+                <img class="prize-img" src="./assets/img/footer/rzetelna-firma.jpg" alt="prize01" width="197" height="85">
+            </div>
+            <div class="footer_top-col--img">
+                <img src="./assets/img/footer/zaufanypartnerwbiznesie.jpg" alt="prize02" width="180" height="57">
+            </div>
+        </div>
     </div>
-    <div class="footer-bottom">
+    <div class="footer_bottom">
         <p>© Spark Promotions Sp. z o.o. S.k. 2010 - <span></span>. All rights reserved.</p>
     </div>
 </div>
