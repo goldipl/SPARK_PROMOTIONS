077b82fe344dd9d12111c2c882d94acc033c6cc3
goldipl <mgodlewski92@gmail.com>
2023-11-02 15:54:33 +0100

    fill leave msg template and style it

--- components/main_page/leave_message.php
+++ components/main_page/leave_message.php
@@ -2,6 +2,26 @@
     <h1 class="title">Zostaw nam <span>wiadomość</span></h1>
     <p class="desc">Napisz do nas</p>
     <div class="leave_message_container">
-        
+        <form>
+            <div class="leave_message_container__top">
+                <div class="leave_message_container__top__left">
+                    <input type="text" name="leave_message_name" id="leave_message_name" placeholder="Twoje imię">
+                </div>
+                <div class="leave_message_container__top__right">
+                    <input type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" name="leave_message_email" id="leave_message_email" placeholder="Twój adres e-mail">
+                </div>
+            </div>
+            <div class="leave_message_container__bottom">
+                <textarea name="leave_message_textarea" id="leave_message_textarea" placeholder="Wiadomość"></textarea>
+            </div>
+            <div class="leave_message_container__button">
+                <div class="captcha">
+                    <img src="./assets/img/contact_component/recaptcha.jpg" alt="captcha" width="308" height="82">
+                </div>
+                <div class="button">
+                    <input class="button" type="submit" value="Wyślij">
+                </div>
+            </div>
+        </form>
     </div>
 </section>
