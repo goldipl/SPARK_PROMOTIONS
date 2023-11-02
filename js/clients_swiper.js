49f9a991bbe650813e0d699c9184b988d9285396
goldipl <mgodlewski92@gmail.com>
2023-11-02 20:13:56 +0100

    change swiper breakpoints

--- js/clients_swiper.js
+++ js/clients_swiper.js
@@ -1,7 +1,7 @@
 const clients_swiper = new Swiper('.clients_swiper', {
     // Optional parameters
     direction: 'horizontal',
-    loop: true,
+    slidesPerView: 7,
   
     // Navigation arrows
     navigation: {
@@ -9,4 +9,23 @@ const clients_swiper = new Swiper('.clients_swiper', {
       prevEl: '.swiper-button-prev',
     },
   
+    breakpoints: {
+      // when window width is >= 1320px
+      1320: {
+        slidesPerView: 7,
+      },
+      // when window width is >= 960px
+      960: {
+        slidesPerView: 5,
+      },
+      // when window width is >= 760px
+      660: {
+        slidesPerView: 4,
+      },
+      // when window width is >= 200px
+      200: {
+        slidesPerView: 3,
+      },
+    },
+
   });
