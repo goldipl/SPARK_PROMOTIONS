4ad6892ca63cafbc43750fd4484a04b11181f95a
goldipl <mgodlewski92@gmail.com>
2023-11-02 16:16:04 +0100

    change and styling aside boxes

--- components/common/aside_boxes.php
+++ components/common/aside_boxes.php
@@ -1,26 +1,8 @@
 <div class="aside_boxes">
     <div class="aside_boxes_slot">
-        <h1 class="title">Promocje</h1>
-        <a href="http://">
-            <div class="aside_img">
-                <img src="./assets/img/recommend/white_green_pen.jpg" alt="white green pen" width="80" height="288">
-            </div>
-            <p class="desc">Długopis <br>AP4515W-09</p>
-        </a>
+
     </div>
     <div class="aside_boxes_slot">
-        <h1 class="title">Ostatnio oglądane</h1>
-        <ol class="aside_boxes_slot--list">
-            <li><a href="">Podkładka na biurko, PU z recyklingu P300.181</a></li>
-            <li><a href="">Organizer na notatnik A5 V2986-04</a></li>
-            <li><a href="">Organizer na biurko, PU z recyklingu P300.221</a></li>
-            <li><a href="">Nóż do listów V2271-04</a></li>
-            <li><a href="">Bambusowy kalendarz na biurko V0261-16</a></li>
-            <li><a href="">Długopis AP4515W-09</a></li>
-            <li><a href="">Długopis El Primero Color EP-COLOR-01</a></li>
-            <li><a href="">Długopis AP1001-09</a></li>
-            <li><a href="">Klip do dokumentów V2794-08</a></li>
-            <li><a href="">Długopis El Primero Solid EP-SOLID-09</a></li>
-        </ol>
+
     </div>
 </div>
