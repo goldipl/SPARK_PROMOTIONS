1386c0427ba65eb48145f37115ab3d138221fd94
goldipl <mgodlewski92@gmail.com>
2023-11-09 18:10:28 +0100

    styling and fill aside banner templates

--- components/common/aside_boxes.php
+++ components/common/aside_boxes.php
@@ -1,8 +1,12 @@
 <div class="aside_boxes">
     <div class="aside_boxes_slot">
-
+        <a href="http://">
+            <img src="./assets/img/banners/vertical_pink_banner.jpg" alt="banner" width="232" height="423">
+        </a>
     </div>
     <div class="aside_boxes_slot">
-
+        <a href="http://">
+            <img src="./assets/img/banners/vertical_green_banner.jpg" alt="banner" width="232" height="423">
+        </a>
     </div>
 </div>
