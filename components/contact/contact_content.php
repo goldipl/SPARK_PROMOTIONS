8057a2ea3a139ec4cfde791bece90395f2f6f4f0
goldipl <mgodlewski92@gmail.com>
2023-11-12 16:19:03 +0100

    add contact starter files

---
+++ components/contact/contact_content.php
@@ -0,0 +1,3 @@
+<div class="contact_container">
+    
+</div>
---
+++ contact.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Spark Promotions | Kontakt</title>
+        <link rel="shortcut icon" href="./assets/icons/favicon.jpg" type="image/x-icon">
+        <link rel="stylesheet" href="./scss/main.css">
+    </head>
+    <body class="contact_page">
+        <header> 
+            <?php include "./components/nav.php"; ?> 
+        </header>
+        <div id="main-wrapper">
+            <div class="breadcrumbs">
+                <ul>
+                    <li class="breadcrumb-item">
+                        <a href="#">Home</a>
+                    </li>
+                    <li class="breadcrumb-item">
+                        <a href="#">Kontakt</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="row__container">
+            <?php include "./components/contact/contact_content.php"; ?> 
+            </div>
+        </div>
+        <footer> 
+            <?php include "./components/footer.php"; ?> 
+        </footer>
+        <script src="./js/mobile_searchbar.js"></script>
+        <script src="./js/script.js"></script>
+    </body>
+</html>
