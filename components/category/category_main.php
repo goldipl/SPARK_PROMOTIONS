710922cd5f811d02e671f3112b3cd7310992eda4
goldipl <mgodlewski92@gmail.com>
2023-11-12 12:05:05 +0100

    fill top otions template & styling it

--- components/category/category_main.php
+++ components/category/category_main.php
@@ -1,6 +1,5 @@
 <div class="category_main">
-    <h1 class="title">Akcesoria biurowe reklamowe</h1>
-    <p class="description">Menadżer ds. reklamy? Specjalista ds. Marketingu? Właściciel małej firmy? To nie ma znaczenia! Promoteca.pl ma do zaproponowania akcesoria biurowe reklamowe dopasowane do różnych potrzeb.</p> 
+    <h1 class="title">Akcesoria biurowe</h1>
     
     <?php include "./components/category/category_top_options.php"; ?> 
     
