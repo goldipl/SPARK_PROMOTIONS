4a64abe6fc243f7e584421f1b4fbedb5cfaa7bf7
goldipl <mgodlewski92@gmail.com>
2023-11-15 20:39:11 +0100

    styling and fill products detail component

--- components/products_card/products_details.php
+++ components/products_card/products_details.php
@@ -1,3 +1,40 @@
 <div class="products_details">
+    <div class="products_details__top">
+        <h1 class="name">Klip do dokumentów</h1>
+        <p class="code">Kod: AXV279410</p>
+        <div class="products-variants">
+            <div class="products-variants--slot">
+                <img src="./assets/img/products_card/small-klip-red.jpg" alt="variant img" width="29" height="50">
+            </div>
+            <div class="products-variants--slot">
+                <img src="./assets/img/products_card/small-klip-blue.jpg" alt="variant img" width="29" height="50">
+            </div>
+            <div class="products-variants--slot">
+                <img src="./assets/img/products_card/small-klip-black.jpg" alt="variant img" width="29" height="50">
+            </div>
+            <div class="products-variants--slot">
+                <img src="./assets/img/products_card/small-klip-yellow.jpg" alt="variant img" width="29" height="50">
+            </div>
+            <div class="products-variants--slot">
+                <img src="./assets/img/products_card/small-klip-green.jpg" alt="variant img" width="29" height="50">
+            </div>
+            <div class="products-variants--slot">
+                <img src="./assets/img/products_card/small-klip-white.jpg" alt="variant img" width="29" height="50">
+            </div>
+            <div class="products-variants--slot active">
+                <img src="./assets/img/products_card/small-klip-orange.jpg" alt="variant img" width="29" height="50">
+            </div>
+            <div class="products-variants--slot">
+                <img src="./assets/img/products_card/small-klip-purple.jpg" alt="variant img" width="29" height="50">
+            </div>
+        </div>
+    </div>
+    <div class="products_details__bottom">
+        <div class="products_details__bottom_left">
 
+        </div>
+        <div class="products_details__bottom_right">
+
+        </div>
+    </div>
 </div>
