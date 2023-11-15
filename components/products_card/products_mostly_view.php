aa412d4bf32093e8552a573801a3a5e2f4df3cc4
goldipl <mgodlewski92@gmail.com>
2023-11-15 20:43:46 +0100

    add & style products mostly view template

--- components/products_card/products_mostly_view.php
+++ components/products_card/products_mostly_view.php
@@ -1,3 +1,117 @@
 <div class="products_mostly_view">
-    
+    <h1 class="title">Najczęściej oglądane</h1>
+    <div class="products_blocks_wrapper">
+        <div class="products_blocks_wrapper__slot new">
+            <div class="products_block_wrapper__slot--img">
+                <img src="./assets/img/products/pendrive.jpg">
+            </div>
+            <div class="products_block_wrapper__slot--price">
+                <p class="price">Cena od: <span>44,00 PLN</span>
+                </p>
+            </div>
+            <div class="products_block_wrapper__slot--title">
+                <p>USB 32GB Pierre Cardin</p>
+            </div>
+            <div class="products_block_wrapper__slot--colors">
+                <div class="color_square black"></div>
+            </div>
+            <div class="products_block_wrapper__slot--btn">
+                <a href="#">
+                    <span class="cart-btn-text">Do Koszyka</span>
+                </a>
+            </div>
+        </div>
+        <div class="products_blocks_wrapper__slot promotion">
+            <div class="products_block_wrapper__slot--img">
+                <img src="./assets/img/products/notepad.jpg">
+            </div>
+            <div class="products_block_wrapper__slot--price">
+                <p class="old-price">Cena od: <span>
+                        <s>97,00 PLN</s>
+                    </span>
+                </p>
+                <p class="price">
+                    <span>59,00 PLN</span>
+                </p>
+            </div>
+            <div class="products_block_wrapper__slot--title">
+                <p>Bambusowy notatnik ok. A6 V2967-16</p>
+            </div>
+            <div class="products_block_wrapper__slot--colors">
+                <div class="color_square black"></div>
+            </div>
+            <div class="products_block_wrapper__slot--btn">
+                <a href="#">
+                    <span class="cart-btn-text">Do Koszyka</span>
+                </a>
+            </div>
+        </div>
+        <div class="products_blocks_wrapper__slot topseller">
+            <div class="products_block_wrapper__slot--img">
+                <img src="./assets/img/products/pen.jpg">
+            </div>
+            <div class="products_block_wrapper__slot--price">
+                <p class="price">Cena od: <span>1,99 PLN</span>
+                </p>
+            </div>
+            <div class="products_block_wrapper__slot--title">
+                <p>Długopis metalowy ASCOT</p>
+            </div>
+            <div class="products_block_wrapper__slot--colors">
+                <div class="color_square black"></div>
+                <div class="color_square red"></div>
+                <div class="color_square white"></div>
+                <div class="color_square green"></div>
+                <div class="color_square orange"></div>
+                <div class="color_square purple"></div>
+                <div class="color_square blue"></div>
+                <div class="color_square grey"></div>
+            </div>
+            <div class="products_block_wrapper__slot--btn">
+                <a href="#">
+                    <span class="cart-btn-text">Do Koszyka</span>
+                </a>
+            </div>
+        </div>
+        <div class="products_blocks_wrapper__slot">
+            <div class="products_block_wrapper__slot--img">
+                <img src="./assets/img/products/notepadA4.jpg">
+            </div>
+            <div class="products_block_wrapper__slot--price">
+                <p class="price">Cena od: <span>48,25 PLN</span>
+                </p>
+            </div>
+            <div class="products_block_wrapper__slot--title">
+                <p>Notatnik A4</p>
+            </div>
+            <div class="products_block_wrapper__slot--colors">
+                <div class="color_square black"></div>
+            </div>
+            <div class="products_block_wrapper__slot--btn">
+                <a href="#">
+                    <span class="cart-btn-text">Do Koszyka</span>
+                </a>
+            </div>
+        </div>
+        <div class="products_blocks_wrapper__slot">
+            <div class="products_block_wrapper__slot--img">
+                <img src="./assets/img/products/colormarkers.jpg">
+            </div>
+            <div class="products_block_wrapper__slot--price">
+                <p class="price">Cena od: <span>9,59 PLN</span>
+                </p>
+            </div>
+            <div class="products_block_wrapper__slot--title">
+                <p>Zakreślacz</p>
+            </div>
+            <div class="products_block_wrapper__slot--colors">
+                <div class="color_square white"></div>
+            </div>
+            <div class="products_block_wrapper__slot--btn">
+                <a href="#">
+                    <span class="cart-btn-text">Do Koszyka</span>
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
