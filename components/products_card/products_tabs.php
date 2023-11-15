1a25529cd704dff467c5d01274ccd3ecc071f8dd
goldipl <mgodlewski92@gmail.com>
2023-11-15 21:14:19 +0100

    fill & styling products tabs

--- components/products_card/products_tabs.php
+++ components/products_card/products_tabs.php
@@ -1,3 +1,32 @@
 <div class="products_tabs">
-    
+    <input type="radio" id="products_tab_1" name="products_tabs" checked="checked">
+    <div class="products_tab-text">
+        <label class="products_label_tab_1" for="products_tab_1">
+            <p>Opis</p>
+        </label>
+    </div>
+    <div class="products_tab">
+        <div class="products_tab_box">
+            <p>Klip do dokumentów</p>
+            <p>Produkt dostępny na zamówienie najwcześniej od 14.04</p>
+        </div>
+    </div>
+    <input type="radio" id="products_tab_2" name="products_tabs">
+    <div class="products_tab-text">
+        <label class="products_label_tab_2" for="products_tab_2">
+            <p>Szczegóły</p>
+        </label>
+    </div>
+    <div class="products_tab">
+        <p>bdfgfgfdgdf 2</p>
+    </div>
+    <input type="radio" id="products_tab_3" name="products_tabs">
+    <div class="products_tab-text">
+        <label class="products_label_tab_3" for="products_tab_3">
+            <p>Dostępność</p>
+        </label>
+    </div>
+    <div class="products_tab">
+        <p>bdfgfgfdgdf 3</p>
+    </div>
 </div>
