1c63d8583f1b65bb8f0f5876af816c2e8800784b
goldipl <mgodlewski92@gmail.com>
2023-11-16 18:40:53 +0100

    fill & styling products tabs

--- components/products_card/products_tabs.php
+++ components/products_card/products_tabs.php
@@ -18,7 +18,60 @@
         </label>
     </div>
     <div class="products_tab">
-        <p>bdfgfgfdgdf 2</p>
+        <table cellspacing="0" cellpadding="0">
+            <tr>
+                <td>Rozmiar</td>
+                <td>3,5 x 5,6 x 1,2 cm</td>
+            </tr>
+            <tr>
+                <td>Waga</td>
+                <td>8,5</td>
+            </tr>
+            <tr>
+                <td>Wysokość</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>Średnica</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>Numer celny</td>
+                <td>83059000</td>
+            </tr>
+            <tr>
+                <td>Pojemność</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>Rozmiar znakowania</td>
+                <td>-</td>
+            </tr>
+            <tr>
+                <td>Opakowanie</td>
+                <td>50 pcs/ bag</td>
+            </tr>
+            <tr>
+                <td>Ilość w opakowaniu</td>
+                <td>małe 500; duże 1000</td>
+            </tr>
+            <tr>
+                <td>Strona w katalogu</td>
+                <td>108</td>
+            </tr>
+            <tr>
+                <td>Kolor</td>
+                <td>Pomarańczowy</td>
+            </tr>
+            <tr>
+                <td>Znakowanie</td>
+                <td>T2 (tampodruk)</td>
+            </tr>
+            <tr>
+                <td>Materiał</td>
+                <td>Plastik, metal</td>
+            </tr>
+        </table>
     </div>
     <input type="radio" id="products_tab_3" name="products_tabs">
     <div class="products_tab-text">
@@ -27,6 +80,71 @@
         </label>
     </div>
     <div class="products_tab">
-        <p>bdfgfgfdgdf 3</p>
+        <div class="availability_status_container">
+            <div class="availability_status_container__slot">
+                <div class="availability_status_container__slot--img">
+                    <img src="./assets/img/products_card/small-klip-red.jpg" alt="variant img" width="29" height="50">
+                </div>
+                <div class="availability_status_container__slot--text">
+                    <p>(1300 sztuk)</p>
+                </div>
+            </div>
+            <div class="availability_status_container__slot">
+                <div class="availability_status_container__slot--img">
+                    <img src="./assets/img/products_card/small-klip-black.jpg" alt="variant img" width="29" height="50">
+                </div>
+                <div class="availability_status_container__slot--text">
+                    <p>(1300 sztuk)</p>
+                </div>
+            </div>
+            <div class="availability_status_container__slot">
+                <div class="availability_status_container__slot--img">
+                    <img src="./assets/img/products_card/small-klip-blue.jpg" alt="variant img" width="29" height="50">
+                </div>
+                <div class="availability_status_container__slot--text">
+                    <p>(1300 sztuk)</p>
+                </div>
+            </div>
+            <div class="availability_status_container__slot">
+                <div class="availability_status_container__slot--img">
+                    <img src="./assets/img/products_card/small-klip-yellow.jpg" alt="variant img" width="29" height="50">
+                </div>
+                <div class="availability_status_container__slot--text">
+                    <p>(1300 sztuk)</p>
+                </div>
+            </div>
+            <div class="availability_status_container__slot">
+                <div class="availability_status_container__slot--img">
+                    <img src="./assets/img/products_card/small-klip-green.jpg" alt="variant img" width="29" height="50">
+                </div>
+                <div class="availability_status_container__slot--text">
+                    <p>(1300 sztuk)</p>
+                </div>
+            </div>
+            <div class="availability_status_container__slot">
+                <div class="availability_status_container__slot--img">
+                    <img src="./assets/img/products_card/small-klip-white.jpg" alt="variant img" width="29" height="50">
+                </div>
+                <div class="availability_status_container__slot--text">
+                    <p>(1300 sztuk)</p>
+                </div>
+            </div>
+            <div class="availability_status_container__slot">
+                <div class="availability_status_container__slot--img">
+                    <img src="./assets/img/products_card/small-klip-orange.jpg" alt="variant img" width="29" height="50">
+                </div>
+                <div class="availability_status_container__slot--text">
+                    <p>(1300 sztuk)</p>
+                </div>
+            </div>
+            <div class="availability_status_container__slot">
+                <div class="availability_status_container__slot--img">
+                    <img src="./assets/img/products_card/small-klip-purple.jpg" alt="variant img" width="29" height="50">
+                </div>
+                <div class="availability_status_container__slot--text">
+                    <p>(1300 sztuk)</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
