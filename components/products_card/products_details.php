e8b1a08b62498a4f20178de32a7dbac29deb5fcd
goldipl <mgodlewski92@gmail.com>
2023-11-16 18:13:46 +0100

    fill products details template & styling

--- components/products_card/products_details.php
+++ components/products_card/products_details.php
@@ -28,13 +28,55 @@
                 <img src="./assets/img/products_card/small-klip-purple.jpg" alt="variant img" width="29" height="50">
             </div>
         </div>
+        <div class="pricing_availability_container">
+            <div class="pricing_availability_container__btn">
+                <button>Wycena i wizualizacja na email</button>
+            </div>
+            <div class="pricing_availability_container__availability">
+                <p>Dostępność: <span class="circle green"></span><span class="grey">(1300 sztuk)</span></p>
+            </div>
+        </div>
     </div>
     <div class="products_details__bottom">
         <div class="products_details__bottom_left">
-
+            <div class="input-slot">
+                <label class="products_quantity" for="products_quantity">Ilość:</label>
+                <input type="number" name="products_quantity" id="products_quantity">
+            </div>
+            <div class="input-slot">
+                <label class="products_branding_method" for="products_branding_method">Metoda znakowania:</label>
+                <select name="products_branding_method" id="products_branding_method">
+                    <option value="Opcja 1">Opcja 1</option>
+                    <option value="Opcja 2">Opcja 2</option>
+                </select>
+            </div>
+            <div class="input-slot">
+                <label class="products_print_colors_quantity" for="products_print_colors_quantity">Ilość kolorów nadruku:</label>
+                <input type="number" name="products_print_colors_quantity" id="products_print_colors_quantity">
+            </div>
+            <div class="input-slot">
+                <label class="products_print_postion" for="products_print_postion">Pozycja nadruku:</label>
+                <select name="products_print_postion" id="products_print_postion">
+                    <option value="Opcja 1">Opcja 1</option>
+                    <option value="Opcja 2">Opcja 2</option>
+                </select>
+            </div>
+            <div class="text-slot upload-files">
+                <p class="title">Wgraj pliki:</p>
+                <p class="desc">Dodaj plik do miejsca znakowania:</p>
+            </div>
+            <div class="add-files">
+                <img class="add-files-img" src="./assets/icons/cart/upload.svg" alt="upload" width="21" height="14">
+                <p class="add-files-text">Przeciągnij i upuść albo <br>Dodaj plik</p>
+            </div>
+            <div class="text-slot">
+                <p class="desc">Wymagamy plik w wektorach <br>format pliku pdf zapisany w krzywych (czcionka i kontury)</p>
+            </div>
         </div>
         <div class="products_details__bottom_right">
-
+            <p class="title">Wyceń i zamów</p>
+            <p class="price">Cena od:<span class="big green">1,66</span><span class="large green">PLN</span></p>
+            <p class="desc">Tu pojawi się cena produktu <br>z wybranym przez Ciebie nadrukiem</p>
         </div>
     </div>
 </div>
