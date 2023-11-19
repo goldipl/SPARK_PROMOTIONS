3c2a325e4e3fdd58af8cd446fd34521d757871b0
goldipl <mgodlewski92@gmail.com>
2023-11-19 11:32:36 +0100

    change html lang to pl

---
+++ components/cart/cart_step_one_content.php
@@ -0,0 +1,126 @@
+<div class="cart_step_one_container">
+    <div class="cart_step_one_container__left">
+        <div class="cart_product_container">
+            <div class="cart_product_container--img">
+                <img src="./assets/img/products_card/Klip-do-dokumentow-V2794-08-2.jpg" alt="product img" width="140" height="140">
+            </div>
+            <div class="cart_product_container--text">
+                <div class="title">
+                    <p>Klip do dokumentów V2794-08</p>
+                </div>
+                <div class="text-slot">
+                    <div class="text-slot--left">
+                        <p>Technika znakowania:</p>
+                    </div>
+                    <div class="text-slot--right">
+                        <p>Tampodruk</p>
+                    </div>
+                </div>
+                <div class="text-slot">
+                    <div class="text-slot--left">
+                        <p>Miejsce znakowań:</p>
+                    </div>
+                    <div class="text-slot--right">
+                        <p>Z jednej strony</p>
+                    </div>
+                </div>
+                <div class="text-slot">
+                    <div class="text-slot--left">
+                        <p>Przewidywana ilość kolorów nadruku:</p>
+                    </div>
+                    <div class="text-slot--right">
+                        <p>2 kolory</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cart_product_container--input">
+                <label for="cart_product_quantity">Ilość produktów</label>
+                <input type="number" name="cart_product_quantity" id="cart_product_quantity" min="0">
+                <div class="cart_product_container--delete">
+                    <p>Usuń</p>
+                </div>
+            </div>
+        </div>
+        <div class="upload_logo_notes_container">
+            <div class="upload_logo_notes_container__left">
+                <div class="title">
+                    <p>Wgraj logo lub projekt:</p>
+                </div>
+                <div class="upload-area">
+                    <div class="upload-area--img">
+                        <img src="./assets/icons/cart/upload.svg" alt="upload" width="21" height="14">
+                    </div>
+                    <div class="upload-area--text">
+                        <p>Przeciągnij i upuść albo <br>
+                            <a href="">Dodaj plik</a>
+                        </p>
+                    </div>
+                    <div class="upload-area--small-text">
+                        <p>Wymagamy plik w wektorach <br>format pliku pdf zapisany w krzywych (czcionka i kontury) </p>
+                    </div>
+                </div>
+            </div>
+            <div class="upload_logo_notes_container__right">
+                <div class="title">
+                    <p>Zostaw notatkę:</p>
+                </div>
+                <div class="text-area">
+                    <textarea name="cart_notes" id="cart_notes"></textarea>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="cart_step_one_container__right">
+        <div class="cart_pay_box">
+            <div class="cart_pay_box__top">
+                <div class="cart_pay_box__top-slot">
+                    <div class="cart_pay_box__top-slot--left">
+                        <p>Wartość produktów</p>
+                    </div>
+                    <div class="cart_pay_box__top-slot--right">
+                        <p>47,00 zł</p>
+                    </div>
+                </div>
+                <div class="cart_pay_box__top-slot">
+                    <div class="cart_pay_box__top-slot--left">
+                        <p>Wartość znakowania</p>
+                    </div>
+                    <div class="cart_pay_box__top-slot--right">
+                        <p>50,00 zł</p>
+                    </div>
+                </div>
+                <div class="cart_pay_box__top-slot">
+                    <div class="cart_pay_box__top-slot--left">
+                        <p>Dostawa:</p>
+                    </div>
+                    <div class="cart_pay_box__top-slot--right">
+                        <p>20,00 zł</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cart_pay_box__bottom">
+                <div class="cart_pay_box__bottom-slot">
+                    <div class="cart_pay_box__bottom-slot--left">
+                        <p>Całkowita cena netto:</p>
+                    </div>
+                    <div class="cart_pay_box__bottom-slot--right">
+                        <p>
+                            <span>200.90</span> zł
+                        </p>
+                    </div>
+                </div>
+                <div class="cart_pay_box__bottom-slot">
+                    <div class="cart_pay_box__bottom-slot--left">
+                        <p>Brutto:</p>
+                    </div>
+                    <div class="cart_pay_box__bottom-slot--right">
+                        <p>220,00 zł</p>
+                    </div>
+                </div>
+            </div>
+            <div class="cart_pay_box__button">
+                <a class="pay-btn" href="">Płacę</a>
+            </div>
+        </div>
+    </div>
+</div>
