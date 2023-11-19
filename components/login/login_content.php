926e9edf81937bfe6739a0f2c23d962d43b1e106
goldipl <mgodlewski92@gmail.com>
2023-11-19 16:16:32 +0100

    add login template

---
+++ components/login/login_content.php
@@ -0,0 +1,26 @@
+<div class="cart_step_two_container login">
+    <div class="cart_step_two_container__box">
+        <div class="cart_step_two_container__box__left">
+            <div class="title">
+                <p>Zaloguj się</p>
+            </div>
+            <div class="input-slot">
+                <input type="email" name="email" placeholder="Adres email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required>
+            </div>
+            <div class="input-slot">
+                <input type="password" name="password" placeholder="Hasło" required>
+            </div>
+            <div class="full-green--btn login">
+                <button>Idź do koszyka</button>
+            </div>
+        </div>
+        <div class="cart_step_two_container__box__right">
+            <div class="title">
+                <p>Nie masz konta?</p>
+            </div>
+            <div class="full-green--btn register">
+                <button>Zarejestruj się</button>
+            </div>
+        </div>
+    </div>
+</div>
