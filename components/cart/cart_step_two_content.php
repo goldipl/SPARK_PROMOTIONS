804f2aafad4967bd187de2a2c964c68c744fed79
goldipl <mgodlewski92@gmail.com>
2023-11-19 15:04:58 +0100

    add cart step two starter files

---
+++ components/cart/cart_step_two_content.php
@@ -0,0 +1,8 @@
+<div class="cart_step_two_container">
+    <div class="cart_step_two_container__box">
+
+    </div>
+    <div class="cart_bottom_button_container">
+        <a class="back-btn" href="">Wróć</a>
+    </div>
+</div>
