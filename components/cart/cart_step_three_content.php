a364695c2e7919f6373bec4648d5b474a6657940
goldipl <mgodlewski92@gmail.com>
2023-11-20 16:44:04 +0100

    fill cart step three sub page & add desktop styles

---
+++ components/cart/cart_step_three_content.php
@@ -0,0 +1,169 @@
+<div class="cart_step_one_container step_three">
+    <div class="cart_step_one_container__left">
+        <div class="cart_products_box">
+            <div class="cart_product_container">
+                <div class="cart_product_container--img">
+                    <img src="./assets/img/products_card/large-klip1.jpg" alt="product img" width="140" height="140">
+                </div>
+                <div class="cart_product_container--text">
+                    <div class="title">
+                        <p>Klip do dokumentów</p>
+                    </div>
+                    <div class="text-slot">
+                        <div class="text-slot--left">
+                            <p>Metoda znakowania:</p>
+                        </div>
+                        <div class="text-slot--right">
+                            <p>Sitodruk</p>
+                        </div>
+                    </div>
+                    <div class="text-slot">
+                        <div class="text-slot--left">
+                            <p>Ilość kolorów nadruku:</p>
+                        </div>
+                        <div class="text-slot--right">
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div class="text-slot">
+                        <div class="text-slot--left">
+                            <p>Pozycja nadruku:</p>
+                        </div>
+                        <div class="text-slot--right">
+                            <p>Front</p>
+                        </div>
+                    </div>
+                    <div class="text-slot products-price">
+                        <div class="text-slot--price-text">
+                            <p>Cena:</p>
+                        </div>
+                        <div class="text-slot--price-value">
+                            <p class="price"><span class="big green">100,66</span><span class="large green">PLN</span></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart_product_container--input">
+                    <label for="cart_product_quantity">Ilość:</label>
+                    <input type="number" name="cart_product_quantity" id="cart_product_quantity" min="0" value="21">
+                </div>
+                <div class="cart_product_container--files">
+                    <p class="desc">Wgraj pliki:</p>
+                    <div class="add-files">
+                        <img class="add-files-img" src="./assets/icons/cart/upload.svg" alt="upload" width="21" height="14">
+                        <p class="add-files-text">Logo.pdf</p>
+                    </div>
+                    <p class="info">Wymagamy plik w wektorach <br>format pliku pdf zapisany w krzywych (czcionka i kontury)</p>
+                </div>
+            </div>
+        </div>
+        <div class="cart_products_box">
+            <div class="cart_product_container">
+                <div class="cart_product_container--img">
+                    <img src="./assets/img/products_card/small-klip-black.jpg" alt="product img" width="140" height="140">
+                </div>
+                <div class="cart_product_container--text">
+                    <div class="title">
+                        <p>Klip do dokumentów</p>
+                    </div>
+                    <div class="text-slot">
+                        <div class="text-slot--left">
+                            <p>Metoda znakowania:</p>
+                        </div>
+                        <div class="text-slot--right">
+                            <p>Sitodruk</p>
+                        </div>
+                    </div>
+                    <div class="text-slot">
+                        <div class="text-slot--left">
+                            <p>Ilość kolorów nadruku:</p>
+                        </div>
+                        <div class="text-slot--right">
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div class="text-slot">
+                        <div class="text-slot--left">
+                            <p>Pozycja nadruku:</p>
+                        </div>
+                        <div class="text-slot--right">
+                            <p>Front</p>
+                        </div>
+                    </div>
+                    <div class="text-slot products-price">
+                        <div class="text-slot--price-text">
+                            <p>Cena:</p>
+                        </div>
+                        <div class="text-slot--price-value">
+                            <p class="price"><span class="big green">100,66</span><span class="large green">PLN</span></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="cart_product_container--input">
+                    <label for="cart_product_quantity">Ilość:</label>
+                    <input type="number" name="cart_product_quantity" id="cart_product_quantity" min="0" value="21">
+                </div>
+                <div class="cart_product_container--files">
+                    <p class="desc">Wgraj pliki:</p>
+                    <div class="add-files">
+                        <img class="add-files-img" src="./assets/icons/cart/upload.svg" alt="upload" width="21" height="14">
+                        <p class="add-files-text">Logo.pdf</p>
+                    </div>
+                    <p class="info">Wymagamy plik w wektorach <br>format pliku pdf zapisany w krzywych (czcionka i kontury)</p>
+                </div>
+            </div>
+        </div>
+        <div class="delivery_invoice_box">
+            <div class="delivery_invoice_box__left">
+                <p class="title">Adres dostawy</p>
+                <p class="desc">Firma 1</p>
+                <p class="desc">Rzeszowska 19</p>
+                <p class="desc">43-472 Kraków</p>
+                <p class="desc">tel. 658 745 111</p>
+            </div>
+            <div class="delivery_invoice_box__right">
+                <p class="title">Dane do faktury</p>
+                <p class="desc">Firma 1</p>
+                <p class="desc">Rzeszowska 19</p>
+                <p class="desc">43-472 Kraków</p>
+                <p class="desc">NIP: 123-456-78-19</p>
+                <p class="desc">tel. 658 745 111</p>
+            </div>
+        </div>
+        <div class="payment_method_box">
+            <p class="payment_method_box__title">Wybierz płatność</p>
+            <div class="payment_method_box__slots">
+                <div class="payment_method_box__slots--slot transfer active">
+                    <input type="radio" name="radio-input" id="radio-input1" checked>
+                    <label class="radio-label" for="radio-input1">Przelew tradycyjny</label>
+                </div>
+                <div class="payment_method_box__slots--slot credit-card">
+                    <input type="radio" name="radio-input" id="radio-input2">
+                    <label class="radio-label" for="radio-input2">Karta płatnicza</label>
+                </div>
+                <div class="payment_method_box__slots--slot online-pay">
+                    <input type="radio" name="radio-input" id="radio-input3">
+                    <label class="radio-label" for="radio-input3">Płatność Online</label>
+                </div>
+            </div>
+        </div>
+        <div class="cart_bottom_button_container">
+            <a class="continue-btn" href="">Wróć</a>
+        </div>
+    </div>
+    <div class="cart_step_one_container__right">
+        <div class="cart_pay_box">
+            <div class="cart_pay_box__content">
+                <p class="desc each-price">Cena za sztukę: 1,66 PLN</p>
+                <p class="desc branding-price">Znakowanie: 222 PLN</p>
+                <p class="desc netto-price"><span>Suma netto:</span>100,66 PLN</p>
+                <p class="desc vat"><span>Podatek VAT 23%:</span>10,66 PLN</p>
+                <p class="desc delivery"><span>Dostawa Kurier:</span>20,00 PLN</p>
+                <p class="desc brutto">Wartość brutto <br>(do zapłaty):</p>
+                <p class="desc brutto-price"><span class="big green">100,66</span><span class="large green">PLN</span</p>
+            </div>
+            <div class="cart_pay_box__button">
+                <a class="next-btn" href="">Dalej</a>
+            </div>
+        </div>
+    </div>
+</div>
