d9ba80ac0df89c07e3ac5e355d9dea477041521c
goldipl <mgodlewski92@gmail.com>
2023-11-20 16:35:22 +0100

    correct products choose colors script

--- js/products_page/product_colors.js
+++ js/products_page/product_colors.js
@@ -1,9 +1,13 @@
-var parentDiv = document.querySelector(".products-variants");
 var colors = document.querySelectorAll(".products_details__top .products-variants--slot");
+
 for (var i = 0; i < colors.length; i++) {
     colors[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
+        // Remove "active" class from all elements
+        colors.forEach(function(element) {
+            element.classList.remove("active");
+        });
+
+        // Add "active" class to the clicked element
+        this.classList.add("active");
+    });
+};
