2e508cd6542406947d838695ba4ae9b83363956d
goldipl <mgodlewski92@gmail.com>
2023-11-21 08:47:34 +0100

    add thank you page php templates & styles

---
+++ components/cart/thank_you_page_content.php
@@ -0,0 +1,12 @@
+<div class="thank_you_page_box">
+    <div class="thank_you_page_box__top">
+        <img class="star-img" src="./assets/icons/cart/star.svg" alt="star" width="57" height="54">
+        <p class="title">Dziękujemy</p>
+        <p class="desc">Przyjęliśmy Twoje zamówienia do realizacji.<br>Numer twojego zamówienia to: <span>3289</span></p>
+    </div>
+    <div class="thank_you_page_box__bottom">
+        <div class="back-button">
+            <a href="./index.php">Do strony głównej</a>
+        </div>
+    </div>
+</div>
