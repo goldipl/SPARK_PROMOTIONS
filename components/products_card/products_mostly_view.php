fe8754108367d17c9d651e556bfc1203b88f3477
goldipl <mgodlewski92@gmail.com>
2023-11-27 21:33:55 +0100

    change products mostly view slots & set text decoration none to link

--- components/products_card/products_mostly_view.php
+++ components/products_card/products_mostly_view.php
@@ -2,18 +2,20 @@
     <h1 class="title">Najczęściej oglądane</h1>
     <div class="products_blocks_wrapper">
         <div class="products_blocks_wrapper__slot new">
-            <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/products/pendrive.jpg">
-            </div>
-            <div class="products_block_wrapper__slot--price">
-                <p class="price">Cena od: <span>44,00 PLN</span>
-                </p>
-            </div>
-            <div class="products_block_wrapper__slot--title">
-                <p>USB 32GB Pierre Cardin</p>
-            </div>
+            <a href="#">
+                <div class="products_block_wrapper__slot--img">
+                    <img src="./assets/img/products/pendrive.jpg">
+                </div>
+                <div class="products_block_wrapper__slot--price">
+                    <p class="price">Cena od: <span>44,00 PLN</span>
+                    </p>
+                </div>
+                <div class="products_block_wrapper__slot--title">
+                    <p>USB 32GB Pierre Cardin</p>
+                </div>
+            </a>
             <div class="products_block_wrapper__slot--colors">
-                <div class="color_square black"></div>
+                <a href="#" class="color_square black"></a>
             </div>
             <div class="products_block_wrapper__slot--btn">
                 <a href="#">
@@ -22,23 +24,25 @@
             </div>
         </div>
         <div class="products_blocks_wrapper__slot promotion">
-            <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/products/notepad.jpg">
-            </div>
-            <div class="products_block_wrapper__slot--price">
-                <p class="old-price">Cena od: <span>
-                        <s>97,00 PLN</s>
-                    </span>
-                </p>
-                <p class="price">
-                    <span>59,00 PLN</span>
-                </p>
-            </div>
-            <div class="products_block_wrapper__slot--title">
-                <p>Bambusowy notatnik ok. A6 V2967-16</p>
-            </div>
+            <a href="#">
+                <div class="products_block_wrapper__slot--img">
+                    <img src="./assets/img/products/notepad.jpg">
+                </div>
+                <div class="products_block_wrapper__slot--price">
+                    <p class="old-price">Cena od: <span>
+                            <s>97,00 PLN</s>
+                        </span>
+                    </p>
+                    <p class="price">
+                        <span>59,00 PLN</span>
+                    </p>
+                </div>
+                <div class="products_block_wrapper__slot--title">
+                    <p>Bambusowy notatnik ok. A6 V2967-16</p>
+                </div>
+            </a>
             <div class="products_block_wrapper__slot--colors">
-                <div class="color_square black"></div>
+                <a href="#" class="color_square black"></a>
             </div>
             <div class="products_block_wrapper__slot--btn">
                 <a href="#">
@@ -47,25 +51,27 @@
             </div>
         </div>
         <div class="products_blocks_wrapper__slot topseller">
-            <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/products/pen.jpg">
-            </div>
-            <div class="products_block_wrapper__slot--price">
-                <p class="price">Cena od: <span>1,99 PLN</span>
-                </p>
-            </div>
-            <div class="products_block_wrapper__slot--title">
-                <p>Długopis metalowy ASCOT</p>
-            </div>
+            <a href="#">
+                <div class="products_block_wrapper__slot--img">
+                    <img src="./assets/img/products/pen.jpg">
+                </div>
+                <div class="products_block_wrapper__slot--price">
+                    <p class="price">Cena od: <span>1,99 PLN</span>
+                    </p>
+                </div>
+                <div class="products_block_wrapper__slot--title">
+                    <p>Długopis metalowy ASCOT</p>
+                </div>
+            </a>
             <div class="products_block_wrapper__slot--colors">
-                <div class="color_square black"></div>
-                <div class="color_square red"></div>
-                <div class="color_square white"></div>
-                <div class="color_square green"></div>
-                <div class="color_square orange"></div>
-                <div class="color_square purple"></div>
-                <div class="color_square blue"></div>
-                <div class="color_square grey"></div>
+                <a href="#" class="color_square black"></a>
+                <a href="#" class="color_square red"></a>
+                <a href="#" class="color_square white"></a>
+                <a href="#" class="color_square green"></a>
+                <a href="#" class="color_square orange"></a>
+                <a href="#" class="color_square purple"></a>
+                <a href="#" class="color_square blue"></a>
+                <a href="#" class="color_square grey"></a>
             </div>
             <div class="products_block_wrapper__slot--btn">
                 <a href="#">
@@ -74,18 +80,20 @@
             </div>
         </div>
         <div class="products_blocks_wrapper__slot">
-            <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/products/notepadA4.jpg">
-            </div>
-            <div class="products_block_wrapper__slot--price">
-                <p class="price">Cena od: <span>48,25 PLN</span>
-                </p>
-            </div>
-            <div class="products_block_wrapper__slot--title">
-                <p>Notatnik A4</p>
-            </div>
+            <a href="#">
+                <div class="products_block_wrapper__slot--img">
+                    <img src="./assets/img/products/notepadA4.jpg">
+                </div>
+                <div class="products_block_wrapper__slot--price">
+                    <p class="price">Cena od: <span>48,25 PLN</span>
+                    </p>
+                </div>
+                <div class="products_block_wrapper__slot--title">
+                    <p>Notatnik A4</p>
+                </div>
+            </a>
             <div class="products_block_wrapper__slot--colors">
-                <div class="color_square black"></div>
+                <a href="#" class="color_square black"></a>
             </div>
             <div class="products_block_wrapper__slot--btn">
                 <a href="#">
@@ -94,18 +102,20 @@
             </div>
         </div>
         <div class="products_blocks_wrapper__slot">
-            <div class="products_block_wrapper__slot--img">
-                <img src="./assets/img/products/colormarkers.jpg">
-            </div>
-            <div class="products_block_wrapper__slot--price">
-                <p class="price">Cena od: <span>9,59 PLN</span>
-                </p>
-            </div>
-            <div class="products_block_wrapper__slot--title">
-                <p>Zakreślacz</p>
-            </div>
+            <a href="#">
+                <div class="products_block_wrapper__slot--img">
+                    <img src="./assets/img/products/colormarkers.jpg">
+                </div>
+                <div class="products_block_wrapper__slot--price">
+                    <p class="price">Cena od: <span>9,59 PLN</span>
+                    </p>
+                </div>
+                <div class="products_block_wrapper__slot--title">
+                    <p>Zakreślacz</p>
+                </div>
+            </a>
             <div class="products_block_wrapper__slot--colors">
-                <div class="color_square white"></div>
+                <a href="#" class="color_square white"></a>
             </div>
             <div class="products_block_wrapper__slot--btn">
                 <a href="#">
