bb5120b6d3d25f7c8c0b6e9e9aa4c59392049f3b
goldipl <mgodlewski92@gmail.com>
2023-11-27 16:49:19 +0100

    set hash link

--- components/main_page/banners.php
+++ components/main_page/banners.php
@@ -5,7 +5,7 @@
                 <img src="./assets/img/banners/horizontal_green_banner.jpg" alt="banner" width="660" height="368">
             </a>
             <div class="button01">
-                <a href="./category.php">Więcej</a>
+                <a href="#">Więcej</a>
             </div>
         </div>
         <div class="banners_container__right">
@@ -13,7 +13,7 @@
                 <img src="./assets/img/banners/horizontal_pink_banner.jpg" alt="banner" width="660" height="368">
            </a>
            <div class="button01">
-                <a href="./category.php">Więcej</a>
+                <a href="#">Więcej</a>
             </div>
         </div>
     </div>
