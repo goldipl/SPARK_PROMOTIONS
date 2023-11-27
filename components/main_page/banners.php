c4b5ec94cc0fd2b2ee5bc0de320112c959cf7982
goldipl <mgodlewski92@gmail.com>
2023-11-27 16:48:35 +0100

    add buttons to banners & styles

--- components/main_page/banners.php
+++ components/main_page/banners.php
@@ -4,11 +4,17 @@
             <a href="#">
                 <img src="./assets/img/banners/horizontal_green_banner.jpg" alt="banner" width="660" height="368">
             </a>
+            <div class="button01">
+                <a href="./category.php">Więcej</a>
+            </div>
         </div>
         <div class="banners_container__right">
            <a href="#">
                 <img src="./assets/img/banners/horizontal_pink_banner.jpg" alt="banner" width="660" height="368">
            </a>
+           <div class="button01">
+                <a href="./category.php">Więcej</a>
+            </div>
         </div>
     </div>
 </section>
