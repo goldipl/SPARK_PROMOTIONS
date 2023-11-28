cbfc75cddc5c7da29ca950cceb34d3372195002b
goldipl <mgodlewski92@gmail.com>
2023-11-28 21:35:25 +0100

    change pagination template and scss

--- components/common/pagination.php
+++ components/common/pagination.php
@@ -1,16 +1,31 @@
 <div class="pagination">
     <div class="pagination_container">
+        <div class="element prev">
+            <img src="./assets/icons/pagination/pagination_right.svg" alt="prev" width="12" height="12">
+        </div>
         <div class="element active">
             <p>1</p>
         </div>
         <div class="element">
             <p>2</p>
         </div>
+        <div class="element">
+            <p>3</p>
+        </div>
+        <div class="element">
+            <p>4</p>
+        </div>
+        <div class="dots">
+            <p>...</p>
+        </div>
+        <div class="element">
+            <p>25</p>
+        </div>
+        <div class="element">
+            <p>26</p>
+        </div>
         <div class="element next">
             <img src="./assets/icons/pagination/pagination_right.svg" alt="next" width="12" height="12">
-        </div>
-        <div class="element last">
-            <img src="./assets/icons/pagination/pagination_last.svg" alt="last" width="12" height="12">
         </div>
     </div>
 </div>
