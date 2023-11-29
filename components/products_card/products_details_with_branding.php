88764d33b21bfb8054c356129cf189338ca07275
goldipl <mgodlewski92@gmail.com>
2023-11-29 13:58:31 +0100

    change products variants to links

--- components/products_card/products_details_with_branding.php
+++ components/products_card/products_details_with_branding.php
@@ -3,30 +3,30 @@
         <h1 class="name">Klip do dokumentów</h1>
         <p class="code">Kod: AXV279410</p>
         <div class="products-variants">
-            <div class="products-variants--slot">
+            <a href="#" class="products-variants--slot">
                 <img src="./assets/img/products_card/small-klip-red.jpg" alt="variant img" width="29" height="50">
-            </div>
-            <div class="products-variants--slot">
+            </a>
+            <a href="#" class="products-variants--slot">
                 <img src="./assets/img/products_card/small-klip-blue.jpg" alt="variant img" width="29" height="50">
-            </div>
-            <div class="products-variants--slot">
+            </a>
+            <a href="#" class="products-variants--slot">
                 <img src="./assets/img/products_card/small-klip-black.jpg" alt="variant img" width="29" height="50">
-            </div>
-            <div class="products-variants--slot">
+            </a>
+            <a href="#" class="products-variants--slot">
                 <img src="./assets/img/products_card/small-klip-yellow.jpg" alt="variant img" width="29" height="50">
-            </div>
-            <div class="products-variants--slot">
+            </a>
+            <a href="#" class="products-variants--slot">
                 <img src="./assets/img/products_card/small-klip-green.jpg" alt="variant img" width="29" height="50">
-            </div>
-            <div class="products-variants--slot">
+            </a>
+            <a href="#" class="products-variants--slot">
                 <img src="./assets/img/products_card/small-klip-white.jpg" alt="variant img" width="29" height="50">
-            </div>
-            <div class="products-variants--slot active">
+            </a>
+            <a href="#" class="products-variants--slot active">
                 <img src="./assets/img/products_card/small-klip-orange.jpg" alt="variant img" width="29" height="50">
-            </div>
-            <div class="products-variants--slot">
+            </a>
+            <a href="#" class="products-variants--slot">
                 <img src="./assets/img/products_card/small-klip-purple.jpg" alt="variant img" width="29" height="50">
-            </div>
+            </a>
         </div>
         <div class="pricing_availability_container">
             <div class="pricing_availability_container__btn">
