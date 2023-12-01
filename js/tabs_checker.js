bdd690bedb3433dfafd2cbd1c2319d868509d7bb
goldipl <mgodlewski92@gmail.com>
2023-12-01 15:42:40 +0100

    refactor code

--- js/tabs_checker.js
+++ js/tabs_checker.js
@@ -18,16 +18,8 @@ arrow_right.addEventListener('click', () => {
 })
 
 arrow_left.addEventListener('click', () => {
-    if (i === 0 || i === 1) {
-        i = 1;
-    } else if (i === 2) {
-        i = 1;
-    } else if (i === 3) {
-        i = 2;
-    } else if (i === 4) {
-        i = 3;
-    } else if (i === 5) {
-        i = 4;
+    if (i >= 0 && i <= 5) {
+        i = Math.max(1, i - 1);
     }
     element = `tab_${i}`;
     document.getElementById(element).checked = true;
