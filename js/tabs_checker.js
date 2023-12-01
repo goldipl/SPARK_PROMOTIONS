9958fd964fae51184ebc678f22a9d3e064c8cbef
goldipl <mgodlewski92@gmail.com>
2023-12-01 16:08:45 +0100

    delete unused constants

--- js/tabs_checker.js
+++ js/tabs_checker.js
@@ -1,10 +1,5 @@
 const arrow_right = document.querySelector('.the_tabs__arrow-right');
 const arrow_left = document.querySelector('.the_tabs__arrow-left');
-const tab_1 = document.getElementById('tab_1');
-const tab_2 = document.getElementById('tab_2');
-const tab_3 = document.getElementById('tab_3');
-const tab_4 = document.getElementById('tab_4');
-const tab_5 = document.getElementById('tab_5');
 const tabs_elements = 5;
 const tabs_elements_minus_one = tabs_elements - 1;
 let i = 1;
@@ -18,7 +13,7 @@ arrow_right.addEventListener('click', () => {
 })
 
 arrow_left.addEventListener('click', () => {
-    if (i >= 0 && i <= 5) {
+    if (i >= 0 && i <= tabs_elements) {
         i = Math.max(1, i - 1);
     }
     element = `tab_${i}`;
